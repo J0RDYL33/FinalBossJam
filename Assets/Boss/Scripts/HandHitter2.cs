c59cf75ab82653aa1d48cfe661fdcb3fa6ca80a0
J0RDYL33 <jordylee264@gmail.com>
2022-08-21 19:38:32 +0100

    Fix push

---
+++ Assets/Boss/Scripts/HandHitter2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandHitter2 : MonoBehaviour
+{
+    private float invulTimer = 0.6f;
+    private BossBehaviourP2 boss;
+    private SpriteRenderer mySR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss = FindObjectOfType<BossBehaviourP2>();
+        mySR = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (invulTimer >= 0)
+            invulTimer -= Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "weapon" && invulTimer <= 0)
+        {
+            StartCoroutine(FlashRed());
+            invulTimer = 0.6f;
+            boss.TakeDamage(1);
+        }
+    }
+
+    IEnumerator FlashRed()
+    {
+        mySR.color = Color.red;
+        yield return new WaitForSeconds(.2f);
+        mySR.color = Color.white;
+    }
+}
